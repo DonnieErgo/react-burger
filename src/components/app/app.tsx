2ab49099cb7884ad2431e3d0e35efcaca034895e
Mikhail Nenashev <nord-vizjerei@yandex.ru>
2022-01-24 15:39:41 +0300

    setup

---
+++ src/components/app/app.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './app.module.css';
+
+class App extends React.Component {
+  render () {
+    return (
+      <>
+      </>
+    )
+  }
+}
+
+export default App;
