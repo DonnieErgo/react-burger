ecf5629cb333822b4f8e2f1e81eb47f7ac70aae5
Mikhail Nenashev <nord-vizjerei@yandex.ru>
2022-01-26 23:29:37 +0300

    final changes

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import AppHeader from '../app-header/app-header';
 import styles from './app.module.css';
 import BurgerIngredients from '../burger-ingredients/burger-ingredients';
@@ -6,18 +5,16 @@ import BurgerConstructor from '../burger-constructor/burger-constructor';
 import dataStatic from '../../utils/data'
 import cartItems from '../../utils/cartItems'
 
-class App extends React.Component {
-  render () {
-    return (
-      <>
-        <AppHeader />
-        <main className={styles.main}>
-          <BurgerIngredients data={dataStatic} />
-          <BurgerConstructor cart={cartItems}/>
-        </main>
-      </>
-    )
-  }
+const App = () => {
+  return (
+    <>
+      <AppHeader />
+      <main className={styles.main}>
+        <BurgerIngredients data={dataStatic} />
+        <BurgerConstructor cart={cartItems}/>
+      </main>
+    </>
+  )
 }
 
 export default App;
